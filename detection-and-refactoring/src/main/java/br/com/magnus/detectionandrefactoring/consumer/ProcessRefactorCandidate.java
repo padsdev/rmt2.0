9e7e67d5eb27c41c7affb42a2be8208b03ff0319
padsdev <fpadilha.augusto@gmail.com>
2026-05-04 17:28:21 -0300

    refactor: update Docker context paths and clean up imports
    - Removed unused import from ProcessRefactorCandidate.java.
    - Updated Docker context paths in docker-compose-full.yml for intermediary, detection, and metrics services to point to the correct directories.

--- detection-and-refactoring/src/main/java/br/com/magnus/detectionandrefactoring/consumer/ProcessRefactorCandidate.java
+++ detection-and-refactoring/src/main/java/br/com/magnus/detectionandrefactoring/consumer/ProcessRefactorCandidate.java
@@ -25,7 +25,6 @@ import org.springframework.util.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
